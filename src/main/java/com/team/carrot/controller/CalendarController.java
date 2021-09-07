1016e5b458145434864e329193dd1038173b54a1
jiyeon <jiyeon@koreait.kr>
2021-09-07 16:52:08 +0900

    주석 추가

--- src/main/java/com/team/carrot/controller/CalendarController.java
+++ src/main/java/com/team/carrot/controller/CalendarController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +18,14 @@ import com.team.carrot.vo.ScheduleVO;
 @RequestMapping("/scheduler/*")
 public class CalendarController {
    
+	private static final Logger logger = LoggerFactory.getLogger(CalendarController.class);
+	
 	@Inject
 	ScheduleService service;
 	
 	@RequestMapping("/calendar")
 	public String Calendar(Model model) throws Exception {
+		logger.info("list");
 		List<ScheduleVO> list = service.readAll();
 		for(ScheduleVO svo : list) {
 			System.out.println(svo);
@@ -30,20 +35,18 @@ public class CalendarController {
 		return "scheduler/calendar";
 	}
 
-   @RequestMapping("/modal")
-   public String modal() {
-	   return "modal";
-   }
-
    @RequestMapping("/modalInsert")
    public String modalInsert(Model model, ScheduleVO vo) throws Exception {
+	   logger.info("insert");
 	   service.create(vo);
+	   System.out.println(vo);
 	   model.addAttribute("vo", vo);
 	   return "redirect:/scheduler/calendar";
    }
 
    @RequestMapping(value = "/delete", method = RequestMethod.GET)
    public String delete(int bno) throws Exception {
+	   logger.info("delete");
 	   System.out.println(service.read(bno));
 	   service.delete(bno);
 	   return "redirect:/scheduler/calendar";
